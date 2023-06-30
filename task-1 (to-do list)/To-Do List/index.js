d55e0077da8c42ebdc58da468051e2d78d63b2aa
Anshumi Kaneria <anshumikaneria2896@gmail.com>
2023-06-30 21:43:33 +0530

    to-do list
    task-1

---
+++ task-1 (to-do list)/To-Do List/index.js
@@ -0,0 +1,78 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+// VANISHING THE TEXT
+document.getElementById("myText").addEventListener("click", function() {
+  this.style.display = "none";
+});
+
+function handleClick(event) {
+  if (event.target.tagName === 'SPAN') {
+    const span = event.target;
+    const cross = document.createElement('div');
+    cross.classList.add('cross');
+    span.appendChild(cross);
+
+    setTimeout(() => {
+      cross.style.transform = 'scaleX(1)';
+      span.style.color = 'transparent';
+      span.style.cursor = 'default';
+    }, 0);
+
+    setTimeout(() => {
+      span.style.display = 'none';
+    }, 1000);
+  }
+}
